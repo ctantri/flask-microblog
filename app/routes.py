2e7181fea01dd9c5c1de388e4d8f0b5b59f6c2cc
Cattleya <19617370+ctantri@users.noreply.github.com>
2022-10-10 00:00:16 +1100

    Complete chapter 2 (templates)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-  return "Hello, World!"
+  user = {"username": "Cattleya"}
+  posts = [
+    {
+      "author": {"username": "Juan"},
+      "body": "Our life is the creation of our mind."
+    },
+    {
+      "author": {"username": "Ayya"},
+      "body": "Everything is mind-made."
+    }
+  ]
+  return render_template("index.html", title="Home", user=user, posts=posts)
